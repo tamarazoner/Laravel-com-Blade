aee68b5ffa180c4817ee8697124f9266dfe8f357
Tamara Zoner <tamara.zoner@gmail.com>
2023-02-24 09:48:02 -0300

    Criação do Index

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Site com GitHub</title>
+</head>
+<body>
+    <h1>Testando, GitHub</h1>
+    <hr>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi necessitatibus harum accusamus autem vitae at temporibus, ullam maiores quidem laudantium. Ipsa consequatur dolorem voluptatem! Qui omnis aspernatur laboriosam in maxime!</p>
+</body>
+</html>
